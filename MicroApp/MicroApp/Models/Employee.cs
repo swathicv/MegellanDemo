fabce9c7ca2fc01f02251a30f6d353095a738b78
Cv, Swathi <swathi.cv@accenture.com>
2018-08-24 12:46:37 +0530

    test proj

---
+++ MicroApp/MicroApp/Models/Employee.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MicroApp.Models
+{
+
+    public class Employee
+    {
+        public int EmployeeId
+        {
+            get;
+            set;
+        }
+        public string EmployeeName
+        {
+            get;
+            set;
+        }
+        public string Address
+        {
+            get;
+            set;
+        }
+        public string Department
+        {
+            get;
+            set;
+        }
+    }
+
+}
